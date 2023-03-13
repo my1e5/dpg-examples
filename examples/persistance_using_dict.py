1c0971a9508f34c1bd0f58f6f720e88517a47442
my1e5 <10064103+my1e5@users.noreply.github.com>
2023-03-13 11:31:30 +0000

    main : small change to persistance using dict

--- examples/persistance_using_dict.py
+++ examples/persistance_using_dict.py
@@ -2,18 +2,29 @@ import json
 import dearpygui.dearpygui as dpg
 dpg.create_context()
 
-try:
-    with open("app-state-dict.json", "r") as f:
-        state = json.load(f)
-except:
-    state = {
+STATE_FILENAME = "app-state-dict.json"
+
+def save_state(state):
+    with open(STATE_FILENAME, "w") as f:
+        json.dump(state, f, indent=4)
+
+def load_state():
+    try:
+        with open(STATE_FILENAME, "r") as f:
+            state = json.load(f)
+    except:
+        state = {
             "name": "My App",
             "settings": {
                 "foo": 0,
                 "bar": "DPG is awesome!",
                 "baz": 3.1415
             }
-        }   
+        }
+    return state
+
+
+state = load_state()
 
 with dpg.window():
     dpg.set_primary_window(dpg.last_item(), True)
@@ -23,13 +34,11 @@ with dpg.window():
     dpg.add_input_float(label="Baz", default_value=state["settings"]["baz"], callback=lambda s, d: state["settings"].__setitem__("baz", d))
     dpg.add_button(label="Print app state", callback=lambda: print(state))
 
-
 dpg.create_viewport(title="App Persistance Demo using Dict", height=400, width=500)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 try:
     dpg.start_dearpygui()
 finally:
-    with open("app-state-dict.json", "w") as f:
-        json.dump(state, f, indent=4)
+    save_state(state)
     dpg.destroy_context()
