f12f8d3b6ac22bb18e355dfe4b1be67d9fa9e8ba
my1e5 <10064103+my1e5@users.noreply.github.com>
2023-03-15 14:19:04 +0000

    main : added updating plot data example

---
+++ plots/plot_update_data.py
@@ -0,0 +1,32 @@
+import dearpygui.dearpygui as dpg
+dpg.create_context()
+
+M = 1
+C = 0
+
+def generate_data(m: float, c: float):
+    data_x, data_y = [], []
+    for x in range(0, 100):
+        data_x.append(x)
+        data_y.append(m*x + c)
+    return data_x, data_y
+
+def update_plot():
+    data_x, data_y = generate_data(dpg.get_value("m_slider"), dpg.get_value("c_slider"))
+    dpg.configure_item('line', x=data_x, y=data_y)
+
+with dpg.window():
+    with dpg.plot(label="y = mx + c", height=400, width=500):
+        xaxis = dpg.add_plot_axis(dpg.mvXAxis, label="x")
+        yaxis = dpg.add_plot_axis(dpg.mvYAxis, label="y")
+        data_x, data_y = generate_data(M, C)
+        dpg.add_line_series(data_x, data_y, tag='line', parent=yaxis)
+        
+    dpg.add_slider_float(label="m", tag="m_slider", default_value=M, min_value=0, max_value=10, callback=update_plot)
+    dpg.add_slider_float(label="c", tag="c_slider", default_value=C, min_value=-50, max_value=50, callback=update_plot)
+
+dpg.create_viewport(width=900, height=600, title='Updating plot data')
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
