8dc02e1ab9cf166777a972efc8c947905c71e13e
my1e5 <10064103+my1e5@users.noreply.github.com>
2023-10-10 13:23:44 +0100

    main : two examples of how to add text to plots

--- plots/plot_text_overlay_using_drawlist.py
+++ plots/plot_text_overlay_using_drawlist.py
@@ -4,17 +4,13 @@ dpg.create_context()
 
 def add_plot_data():
     dpg.add_scatter_series([random() for i in range(10)], [random() for i in range(10)], parent="x-axis")
-    dpg.set_axis_limits_auto("x-axis") # unlock axis limits
-    dpg.set_axis_limits_auto("y-axis")
-    dpg.hide_item("no_data_annotation")
+    dpg.hide_item("overlay_text")
 
 def clear_plot():
     dpg.delete_item("x-axis", children_only=True)
-    dpg.set_axis_limits("x-axis", 0, 1) 
-    dpg.set_axis_limits("y-axis", 0, 1)
-    dpg.show_item("no_data_annotation")
+    dpg.show_item("overlay_text")
 
-with dpg.window():
+with dpg.window(tag="primary_window"):
     dpg.set_primary_window(dpg.last_item(), True)
 
     with dpg.group(horizontal=True):
@@ -25,11 +21,23 @@ with dpg.window():
         with dpg.plot(width=-1, height=-1, tag="plot"):
             dpg.add_plot_axis(dpg.mvXAxis, label="x-axis", tag="x-axis")
             dpg.add_plot_axis(dpg.mvYAxis, label="y-axis", tag="y-axis")
-            dpg.set_axis_limits("x-axis", 0, 1) # lock axis limits so annotation is centered
-            dpg.set_axis_limits("y-axis", 0, 1)
-            dpg.add_plot_annotation(label="No Data Available", default_value=(0.5, 0.5), tag="no_data_annotation")
-  
-dpg.create_viewport(width=800, height=600, title="Plot text annotations")
+
+def set_overlay_text_position():
+    x,y = dpg.get_item_pos("plot")
+    w,h = dpg.get_item_rect_size("plot")
+    x_offset = -38 # needs a little manual adjustment to center
+    y_offset = -25
+    dpg.configure_item("overlay_text", pos=(x + w/2 + x_offset, y + h/2 + y_offset))
+
+with dpg.viewport_drawlist():
+    dpg.draw_text(pos=(100, 100), text="No Data Available", size=13, tag="overlay_text")
+
+with dpg.item_handler_registry() as registry:
+    dpg.add_item_resize_handler(callback=set_overlay_text_position)
+dpg.bind_item_handler_registry("primary_window", registry) 
+
+
+dpg.create_viewport(width=800, height=600, title="Plot text using drawlist overlay")
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
